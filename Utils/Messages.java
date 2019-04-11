bbe8f53c7c4ecda5bdf96b3f1f829db30a965a6b
Flubbii <Fackelmeyer@web.de>
2019-04-11 23:12:58 +0200

    Update Messages.java

--- Utils/Messages.java
+++ Utils/Messages.java
@@ -7,25 +7,31 @@ public class Messages {
 
 	public static void create() {
 		if (cfg.getString("Config.Permission.Command") == null) {
-			cfg.set("Config.Permission.Command", "ChatClear.Use");
+			cfg.set("Config.Permission.Command", "GlobalMute.Use");
 		}
 		if (cfg.getString("Config.Permission.ByPass") == null) {
-			cfg.set("Config.Permission.ByPass", "ChatClear.ByPass");
+			cfg.set("Config.Permission.ByPass", "GlobalMute.ByPass");
 		}
 		if (cfg.getString("Config.Prefix") == null) {
-			cfg.set("Config.Prefix", "&8[&eChatClear&8]&7");
-		}
-		if (cfg.getString("Config.Message.1") == null) {
-			cfg.set("Config.Message.1", "%Prefix% The chat was cleared by &a%who%&7.");
-		}
-		if (cfg.getString("Config.Message.2") == null) {
-			cfg.set("Config.Message.2", "%Prefix% Reason: &e%Reason%");
+			cfg.set("Config.Prefix", "&8[&eGlobalMute&8]&7");
 		}
 		if (cfg.getString("Config.NoPermission") == null) {
 			cfg.set("Config.NoPermission", "%Prefix% &cYou don't have the permission to use this command.");
 		}
-		if (cfg.getString("Config.Activated") == null) {
-			cfg.set("Config.Activated", "%Prefix% &cThe chat is currently deactivated!");
+		if (cfg.getString("Config.Activated.1") == null) {
+			cfg.set("Config.Activated", "%Prefix% &cThe chat was &4deactivated &7by &e%who%&7!");
+		}
+		if (cfg.getString("Config.Activated.2") == null) {
+			cfg.set("Config.Activated.2", "%Prefix% Reason: &e%Reason%");
+		}
+		if (cfg.getString("Config.Deactivated.1") == null) {
+			cfg.set("Config.Activated", "%Prefix% &cThe chat was &2activated &cby &e%who%&7!");
+		}
+		if (cfg.getString("Config.Deactivated.2") == null) {
+			cfg.set("Config.Deactivated.2", "%Prefix% Reason: &e%Reason%");
+		}
+		if (cfg.getString("Config.Inactive") == null) {
+			cfg.set("Config.Inactive", "%Prefix% The chat is currently deactivated");
 		}
 		Config.save();
 	}
